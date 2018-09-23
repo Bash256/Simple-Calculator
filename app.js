47195e4b96570c0f9b48b4f18fee7bdb88e00680
Bash256 <bash.k.256@gmail.com>
2018-09-23 17:34:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+function Addition(){
+    "use strict"
+    var Var1 = document.getElementById('Var1').value;
+    var Var2 = document.getElementById('Var2').value;
+    var Addition = Var1 + Var2;
+    alert(Addition);
+
+};
+function Subtraction(){
+    "use strict"
+    var Var1 = document.getElementById('Var1').value;
+    var Var2 = document.getElementById('Var2').value;
+    var Subtraction = Var1 - Var2;
+    alert(Subtraction);
+
+};
+function Multiplication(){
+    "use strict"
+    var Var1 = document.getElementById('Var1').value;
+    var Var2 = document.getElementById('Var2').value;
+    var Multiplication = Var1 * Var2;
+    alert(Multiplication);
+
+};
+function Division(){
+    "use strict"
+    var Var1 = document.getElementById('Var1').value;
+    var Var2 = document.getElementById('Var2').value;
+    var Division = Var1 / Var2;
+    alert(Division);
+
+}
